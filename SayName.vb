3a04e0a10f2c2eb95718cb52101f37a611548e4f
Brayden Peck <brayden.peck2@gmail.com>
2021-01-22 12:57:42 -0700

    Add project files.

---
+++ SayName.vb
@@ -0,0 +1,22 @@
+﻿'Brayden Peck
+'RCET0265
+'Spring 2021
+'Say My Name
+'
+
+Option Strict On
+Option Explicit On
+
+Module SayName
+
+    Sub Main()
+        Dim name As String
+
+        Console.WriteLine("Please type your name.")
+        name = Console.ReadLine()
+        Console.WriteLine($"Hello, " & name & "! Have a great day!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
